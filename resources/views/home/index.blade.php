2b3d2ee828af35a079123a8f8197916d428fe5f1
emscomplete <Medic!283>
2020-06-18 09:02:53 -0400

    Initial Upload

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -777,18 +777,8 @@
         <a href="javascript:;" data-click="theme-panel-expand" class="theme-collapse-btn"><i class="fa fa-cog"></i></a>
         <div class="theme-panel-content">
             <ul class="theme-list clearfix">
-                <li><a href="javascript:;" class="bg-red" data-theme="red" data-theme-file="/assets/css/theme/red.min.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Red" data-original-title="" title="">&nbsp;</a></li>
-                <li><a href="javascript:;" class="bg-pink" data-theme="pink" data-theme-file="/assets/css/theme/pink.min.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Pink" data-original-title="" title="">&nbsp;</a></li>
-                <li><a href="javascript:;" class="bg-orange" data-theme="orange" data-theme-file="/assets/css/theme/orange.min.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Orange" data-original-title="" title="">&nbsp;</a></li>
-                <li><a href="javascript:;" class="bg-yellow" data-theme="yellow" data-theme-file="/assets/css/theme/yellow.min.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Yellow" data-original-title="" title="">&nbsp;</a></li>
-                <li><a href="javascript:;" class="bg-lime" data-theme="lime" data-theme-file="/assets/css/theme/lime.min.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Lime" data-original-title="" title="">&nbsp;</a></li>
-                <li><a href="javascript:;" class="bg-green" data-theme="green" data-theme-file="/assets/css/theme/green.min.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Green" data-original-title="" title="">&nbsp;</a></li>
-                <li class="active"><a href="javascript:;" class="bg-teal" data-theme-file="/assets/css/theme/teal.min.css" data-theme="theme-selector" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Default" data-original-title="" title="">&nbsp;</a></li>
-                <li><a href="javascript:;" class="bg-aqua" data-theme="aqua" data-theme-file="/assets/css/theme/aqua.min.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Aqua" data-original-title="" title="">&nbsp;</a></li>
-                <li><a href="javascript:;" class="bg-blue" data-theme="blue" data-theme-file="/assets/css/theme/blue.min.css" data-click="default" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Blue" data-original-title="" title="">&nbsp;</a></li>
-                <li><a href="javascript:;" class="bg-purple" data-theme="purple" data-theme-file="/assets/css/theme/purple.min.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Purple" data-original-title="" title="">&nbsp;</a></li>
-                <li><a href="javascript:;" class="bg-indigo" data-theme="indigo" data-theme-file="/assets/css/theme/indigo.min.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Indigo" data-original-title="" title="">&nbsp;</a></li>
-                <li><a href="javascript:;" class="bg-black" data-theme="black" data-theme-file="/assets/css/theme/black.min.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Black" data-original-title="" title="">&nbsp;</a></li>
+                <li class="active"><a href="javascript:;" class="bg-blue" data-theme="blue" data-theme-file="/assets/css/theme/blue.min.css" data-click="default" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Blue" data-original-title="" title="">&nbsp;</a></li>
+
             </ul>
         </div>
     </div>
