8376b4b007affa612ddf2739e9db9397b37327f3
emscomplete <Medic!283>
2020-08-10 21:13:20 -0400

    Initial Upload

--- resources/views/emails/estimate.blade.php
+++ resources/views/emails/estimate.blade.php
@@ -141,17 +141,14 @@
             <table class="content" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
 
 
-                <tr>
-                    <td>SubTotal</td>
-                    <td></td>
-                </tr>
+
                 <tr>
                     <td>Total</td>
-                    <td></td>
+                    <td>{{$estimate->total}}</td>
                 </tr>
                 <tr>
                     <td>Security Deposit</td>
-                    <td></td>
+                    <td>{{$estimate->deposit ?? '$0.00'}}</td>
                 </tr>
 
             </table>
