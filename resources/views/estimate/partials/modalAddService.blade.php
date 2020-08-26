86ce3c9133c253701c7894ccc702fa714dcd3505
emscomplete <Medic!283>
2020-08-26 16:51:04 -0400

    Initial Upload

---
+++ resources/views/estimate/partials/modalAddService.blade.php
@@ -0,0 +1,26 @@
+<!-- Modal -->
+<div class="modal fade" id="addServicesModal" tabindex="-1" role="dialog" aria-labelledby="addServicesModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addServicesModalLabel">Add Service To Package</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true" class="text-danger">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body" id="addServices">
+                <div class="d-flex justify-content-center" id="addServicesSpinner">
+                    <div class="spinner-border text-warning" style="width: 6rem; height: 6rem;" role="status">
+                        <span class="sr-only text-center">Loading...</span>
+                    </div>
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
+
+            </div>
+        </div>
+    </div>
+</div>
