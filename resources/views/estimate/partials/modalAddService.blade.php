417fa33dbe123b64917a27fae76d014733941d4f
emscomplete <Medic!283>
2020-08-26 17:54:08 -0400

    Initial Upload

--- resources/views/estimate/partials/modalAddService.blade.php
+++ resources/views/estimate/partials/modalAddService.blade.php
@@ -12,7 +12,7 @@
                 <form action="#" method="POST">
                     <div class="form-group mb-2">
                         <label for="discountType"> Select Service  <span class="text-danger">*</span></label>
-                        <select class="js-example-basic-single" name="serviceId" style="width:100%" onchange="serviceUpdate()" required>
+                        <select class="js-example-basic-single" name="serviceId" id="serviceId" style="width:100%" onchange="serviceUpdate()" required>
                             <option value="0">Custom Service</option>
                             <option value="1">Other Options</option>
                         </select>
