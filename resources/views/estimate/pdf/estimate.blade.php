6a3b7e11b8115d1af742ca011a4a00c8cbf1da64
emscomplete <Medic!283>
2020-08-30 16:42:35 -0400

    Initial Upload

--- resources/views/estimate/pdf/estimate.blade.php
+++ resources/views/estimate/pdf/estimate.blade.php
@@ -65,7 +65,7 @@
 
     <div class="row">
         <div class="col-12">
-            Reported Problem: {{$estimate->problem ?? ''}}
+            <small> <strong> Reported Problem: </strong> {{$estimate->problem ?? ''}} </small>
         </div>
     </div>
 
