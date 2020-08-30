674fbac9c568743f89da331397a1da5639678036
emscomplete <Medic!283>
2020-08-30 16:51:53 -0400

    Initial Upload

--- resources/views/estimate/partials/problem.blade.php
+++ resources/views/estimate/partials/problem.blade.php
@@ -1,5 +1,14 @@
-<form>
-    <textarea id="mytextarea" name="mytextarea">
-      {{$estimate->problem ?? ''}}
-    </textarea>
-</form>
+<div class="col-xl-12">
+    <form>
+        <div class="col-xl-12 mb-2">
+            <label>Reported Problem</label>
+            <textarea  id="mytextarea" name="mytextarea" style="width: 100%">
+              {{$estimate->problem ?? ''}}
+            </textarea>
+        </div>
+        <div class="col-xl-12">
+            <button type="submit" class="btn btn-primary btn-sm btn-block">Update Reported Problem</button>
+        </div>
+    </form>
+</div>
+
