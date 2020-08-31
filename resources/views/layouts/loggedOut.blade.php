9f8b197cee1cce0f31f8caac4ba2ccfbaef549d6
emscomplete <Medic!283>
2020-08-30 22:39:57 -0400

    Initial Upload

--- resources/views/layouts/loggedOut.blade.php
+++ resources/views/layouts/loggedOut.blade.php
@@ -24,9 +24,9 @@
     <link href="/assets/css/theme/blue.min.css" id="theme" rel="stylesheet" />
 
 
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <link type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/south-street/jquery-ui.css" rel="stylesheet">
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <link type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/south-street/jquery-ui.css" rel="stylesheet">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 
 
 
