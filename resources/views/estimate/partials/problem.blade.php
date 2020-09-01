0f04f19e05aeb56b063083041d6868edc260f332
emscomplete <Medic!283>
2020-09-01 18:27:36 -0400

    Initial Upload

--- resources/views/estimate/partials/problem.blade.php
+++ resources/views/estimate/partials/problem.blade.php
@@ -1,6 +1,7 @@
 <div class="col-xl-12">
     <form action="/updateProblem/{{$estimate->id}}">
         <div class="col-xl-12 mb-2">
+            @csrf
             <label>Reported Problem</label>
             <textarea  id="mytextarea" name="problem" style="width: 100%">
               {{$estimate->problem ?? ''}}
