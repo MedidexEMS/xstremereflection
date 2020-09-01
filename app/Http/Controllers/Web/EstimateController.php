2ae354f3cc7c85245314819704fdb9d100677ebc
emscomplete <Medic!283>
2020-08-31 22:51:36 -0400

    Initial Upload

--- app/Http/Controllers/Web/EstimateController.php
+++ app/Http/Controllers/Web/EstimateController.php
@@ -109,7 +109,7 @@ class EstimateController extends Controller
     {
         $estimate = Estimate::with('customer', 'services', 'packages')->find($id);
 
-        Mail::to('jblevins@xtremereflection.app')->send(new EstimateMailable($estimate));
+        Mail::to([$estimate->customer->email, 'jblevins@xtremereflection.app'])->send(new EstimateMailable($estimate));
 
         if(Mail::failures()){
             $tracking = new EstimateTracking;
