2d31e7e286e4e4eb7de43ec7ac80fea02186e04a
emscomplete <Medic!283>
2020-09-01 20:57:50 -0400

    Initial Upload

--- resources/views/estimate/partials/notes.blade.php
+++ resources/views/estimate/partials/notes.blade.php
@@ -2,7 +2,7 @@
     <div class="card-body py-0 px-0 px-sm-3">
         <h2>Notes or Actions</h2>
 
-
+        <button class="btn btn-primary btn-block" data-toggle="modal" data-target="#noteModal"> Add New Note</button>
             <ul class="list-group">
                 @foreach($estimate->tracking as $note)
                 <li class="list-group-item d-flex justify-content-between align-items-center">
