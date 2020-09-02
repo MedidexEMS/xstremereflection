eb39ea8ca2b168d69680a557613538a8c28a965c
emscomplete <Medic!283>
2020-09-01 22:55:07 -0400

    Initial Upload

--- resources/views/workorder/partials/jobStats.blade.php
+++ resources/views/workorder/partials/jobStats.blade.php
@@ -4,11 +4,13 @@
         <th>Product</th>
         <th>Labor</th>
         <th>Labor %</th>
+        <th>Acquisition</th>
         <th>Gross Profit</th>
         <th>Margin</th>
         <th>Markup</th>
     </tr>
     <tr>
+        <td></td>
         <td></td>
         <td></td>
         <td></td>
