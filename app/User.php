e157236c1e4c6bc8abd40bd35e1175fc7c309bbb
emscomplete <Medic!283>
2020-09-04 12:38:07 -0400

    Initial Upload

--- app/User.php
+++ app/User.php
@@ -25,7 +25,8 @@ class User extends Authenticatable implements TwoFactorAuthenticatableContract, 
         AuthorizationUserTrait,
         Notifiable,
         CanImpersonateUsers,
-        HasApiTokens;
+        HasApiTokens,
+        Notifiable;
 
     protected $presenter = UserPresenter::class;
 
