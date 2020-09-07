e638fbfbd5aa02e141f736c843a42ff050ca2e71
emscomplete <Medic!283>
2020-09-06 20:42:15 -0400

    Initial Upload

--- resources/views/workorder/partials/statusButtons.blade.php
+++ resources/views/workorder/partials/statusButtons.blade.php
@@ -1,4 +1,4 @@
-@if($workOrder->estimate->detailType == 1)
+@if($workOrder->estimate->detailType == 2)
 
     @if($workOrder->status == 1)
         <div class="row mb-4">
@@ -71,45 +71,5 @@
     @endif
 @else
 
-    @if($workOrder->status == 1)
-        <div class="row mb-4">
-            <div class="col">
-                <a href="/workrder/customerapproval/{{$workOrder->id}}" ><button type="button" class="btn btn-primary" style="width: 100%">Tech Enroute</button></a>
-            </div>
-            <div class="col">
-                <button type="button"  class="btn btn-danger" style="width: 100%">Cancel</button>
-            </div>
-        </div>
-    @elseif($workOrder->status == 5)
-        <div class="row mb-4">
-            <div class="col">
-                <a href="/workrder/working/{{$workOrder->id}}" ><button type="button" class="btn btn-primary" style="width: 100%">Tech Enroute</button></a>
-            </div>
-            <div class="col">
-                <button type="button"  class="btn btn-danger" style="width: 100%">Cancel</button>
-            </div>
-        </div>
-    @elseif($workOrder->status == 6)
-        <div class="row mb-4">
-            <div class="col">
-                <a href="/workrder/customerinspection/{{$workOrder->id}}" ><button type="button" class="btn btn-primary" style="width: 100%">Tech Enroute</button></a>
-            </div>
-            <div class="col">
-                <button type="button"  class="btn btn-danger" style="width: 100%">Cancel</button>
-            </div>
-        </div>
-    @elseif($workOrder->status == 7)
-        <div class="row mb-4">
-            <div class="col">
-                <a href="/workrder/invoice/{{$workOrder->id}}" ><button type="button" class="btn btn-primary" style="width: 100%">Tech Enroute</button></a>
-            </div>
-            <div class="col">
-                <button type="button"  class="btn btn-danger" style="width: 100%">Cancel</button>
-            </div>
-        </div>
-    @elseif($workOrder->status == 8)
-        <div class="row mb-4 text-center">
-            <h2>Work Order Has Been Completed</h2>
-        </div>
-    @endif
+
 @endif
