936869e46c66808d3eb360e067730a43940265dd
emscomplete <Medic!283>
2020-09-08 16:26:50 -0400

    Initial Upload

--- resources/views/estimate/payment.blade.php
+++ resources/views/estimate/payment.blade.php
@@ -17,8 +17,6 @@
     @include('partials.messages')
 
 <div class="row">
-    <div class="title m-b-md">Payment Information</div>
-
     <div class="col-xl-6">
         <div class="card">
             <div class="card-header">Payment Information</div>
@@ -27,24 +25,27 @@
                 <p>Total Invoice: ${{$invoice->total}}</p>
                 <p>Total Deposit Due Today: ${{$invoice->deposit}}</p>
             </div>
+            <div class="card-footer">
+                <div class="links">
+                    <form action="/api/payment" method="POST">
+                        <script
+                            src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                            data-key="pk_live_51HPAurAzOqvz8nyKdJdyCFUNhvslEF9mYBTHVfgJgVinnUoZo3SBqyUQm5ZIP8KW6WtGlHtTBOfYEZiZ7jqowTQU00xFw2muvH"
+                            data-amount="{{$invoice->deposit}}"
+                            data-name="{{$invoice->estimate->customer->firstName}} {{$invoice->estimate->customer->lastName}}"
+                            data-description="Deposit Funds"
+                            data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+                            data-currency="usd"></script>
+
+                    </form>
+                </div>
+            </div>
         </div>
 
     </div>
 
 
-    <div class="links">
-        <form action="/api/payment" method="POST">
-            <script
-                src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-                data-key="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"
-                data-amount="{{$invoice->deposit}}"
-                data-name="{{$invoice->estimate->customer->firstName}} {{$invoice->estimate->customer->lastName}}"
-                data-description="Deposit Funds"
-                data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-                data-currency="usd"></script>
 
-        </form>
-    </div>
 </div>
 
 @stop
