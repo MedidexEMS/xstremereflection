c4b4e5d559d0fc1d3f4ecf77c8bd8c7064191f04
emscomplete <Medic!283>
2020-09-11 15:38:19 -0400

    Initial Upload

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -1,6 +1,7 @@
 @if(isset ($errors) && count($errors) > 0)
-    <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
-        <!-- Position it -->
+    <div class="toast" data-autohide="false" style="position: absolute; top: 0; right: 0;">
+        <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+            <!-- Position it -->
             <div class="toast" style="position: absolute; top: 0; right: 0;" role="alert" aria-live="assertive" aria-atomic="true" data-delay="15000">
                 <div class="toast-header bg-danger text-white">
                     <img src="{{url('/assets/img/redsquare.png')}}" class="rounded bg-danger mr-2" alt="..." width="15px" height="15px">
@@ -16,6 +17,8 @@
                 </div>
             </div>
         </div>
+    </div>
+
 
 @endif
 
@@ -25,27 +28,31 @@
 
 
                 @foreach ($data as $msg)
-                    <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
-                        <!-- Position it -->
-                    <div class="toast" style="position: absolute; top: 0; right: 0;" role="alert" aria-live="assertive" aria-atomic="true" data-delay="15000">
-                        <div class="toast-header">
-                            <img src="{{url('/assets/img/bluesquare.png')}}" class="rounded mr-2" alt="..." width="15px" height="15px">
-                            <strong class="mr-auto">Action Completed</strong>
-                            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="toast-body">
-                            {{ $msg }}
+                    <div class="toast" data-autohide="false" style="position: absolute; top: 0; right: 0;">
+                        <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+                            <!-- Position it -->
+                            <div class="toast" style="position: absolute; top: 0; right: 0;" role="alert" aria-live="assertive" aria-atomic="true" data-delay="15000">
+                                <div class="toast-header">
+                                    <img src="{{url('/assets/img/bluesquare.png')}}" class="rounded mr-2" alt="..." width="15px" height="15px">
+                                    <strong class="mr-auto">Action Completed</strong>
+                                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="toast-body">
+                                    {{ $msg }}
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    </div>
+
                 @endforeach
 
 
     @else
-        <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
-            <!-- Position it -->
+        <div class="toast" data-autohide="false" style="position: absolute; top: 0; right: 0;">
+            <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+                <!-- Position it -->
                 <div class="toast" style="position: absolute; top: 0; right: 0;" role="alert" aria-live="assertive" aria-atomic="true" data-delay="15000">
                     <div class="toast-header">
                         <img src="{{url('/assets/img/bluesquare.png')}}" class="rounded mr-2" alt="..." width="15px" height="15px">
@@ -58,6 +65,8 @@
                         {{ $data }}
                     </div>
                 </div>
+            </div>
         </div>
+
     @endif
 @endif
