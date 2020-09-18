5cfdbf0b78aa5910a59ab01a7a7282a2a47b792e
emscomplete <Medic!283>
2020-09-17 20:20:43 -0400

    Initial Upload

--- resources/views/workorder/partials/services.blade.php
+++ resources/views/workorder/partials/services.blade.php
@@ -3,21 +3,21 @@
     <div class="card">
         <div class="card-body py-0 px-0 px-sm-3">
             <div class="list-wrapper">
-                <ul class="d-flex flex-column todo-list todo-list-custom">
+                <ul class="list-group list-unstyled">
 
                     <li>
                         <div class="col-xl-2 text-center">
 
                         </div>
                         <div class="col-xl-10 text-center">
-                            Service To Be Completed
+                            <strong>Service To Be Completed</strong>
                         </div>
 
                     </li>
                     @if($workOrder->services)
                         @foreach($workOrder->services as $s)
                             @if($s->service->serviceTypeId == 1)
-                            <li>
+                            <li class="list-group-item">
                                 <div class="col-xl-2 text-center">
                                     <div class="custom-control custom-checkbox">
 
@@ -25,7 +25,7 @@
 
                                     </div>
                                 </div>
-                                <div class="col-xl-10">
+                                <div class="col-xl-9">
                                    @if($s->service->id == 39) {{$s->description}}   @else {{$s->status}} {{$s->service->description ?? 'Unknown Service'}} @endif
                                 </div>
 
@@ -53,26 +53,28 @@
     <div class="card">
         <div class="card-body py-0 px-0 px-sm-3">
             <div class="list-wrapper">
-                <ul class="d-flex flex-column list-unstyled">
+                <ul class="list-group list-unstyled">
 
                     <li>
                         <div class="col-xl-2 text-center">
 
                         </div>
                         <div class="col-xl-10 text-center">
-                            Service To Be Completed
+                            <strong>Service To Be Completed</strong>
                         </div>
 
                     </li>
                     @if($workOrder->services)
                         @foreach($workOrder->services as $s)
                             @if($s->service->serviceTypeId == 2)
-                                <li>
+                                <li class="list-group-item">
                                     <div class="col-xl-2 text-center">
-                                        <div class="custom-control custom-checkbox">
-                                                <input type="checkbox" name="service" value="{{$s->id}}" class="custom-control-input" data-id="{{$s->id}}" @if($s->status == 2) checked @endif">
-                                        </div>
+                                    <div class="custom-control custom-checkbox">
+
+                                        <input type="checkbox" name="service" value="{{$s->id}}" class="form-check-input" data-id="{{$s->id}}"  @if($s->status == 2) checked @endif>
+
                                     </div>
+            </div>
                                     <div class="col-xl-10">
                                         @if($s->service->id == 39) {{$s->description}}   @else {{$s->service->description ?? 'Unknown Service'}} @endif
                                     </div>
@@ -101,21 +103,21 @@
     <div class="card">
         <div class="card-body py-0 px-0 px-sm-3">
             <div class="list-wrapper">
-                <ul class="d-flex flex-column list-unstyled">
+                <ul class="list-group list-unstyled">
 
                     <li>
                         <div class="col-xl-2 text-center">
 
                         </div>
                         <div class="col-xl-10 text-center">
-                            Service To Be Completed
+                            <strong>Service To Be Completed</strong>
                         </div>
 
                     </li>
                     @if($workOrder->services)
                         @foreach($workOrder->services as $s)
                             @if($s->service->serviceTypeId == 0)
-                                <li>
+                                <li class="list-group-item">
                                     <div class="col-xl-2 text-center">
                                         <div class="custom-control custom-checkbox">
                                             <input type="checkbox" name="service" value="{{$s->id}}" class="custom-control-input" data-id="{{$s->id}}" @if($s->status == 2) checked @endif >
