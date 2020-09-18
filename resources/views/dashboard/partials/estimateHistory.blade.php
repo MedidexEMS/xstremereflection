670a8b3a8ec424574833a5c11798a277d1b9c13b
emscomplete <Medic!283>
2020-09-17 21:08:29 -0400

    Initial Upload

--- resources/views/dashboard/partials/estimateHistory.blade.php
+++ resources/views/dashboard/partials/estimateHistory.blade.php
@@ -8,7 +8,7 @@
                 @foreach($estimateHistory as $row)
                     <li class="feed-item {{$row->statusInfo->color ?? 'feed-item-info'}} ">
                         <time class="date" datetime="{{\Carbon\Carbon::parse($row->created_at)->format('m-d')}}">{{\Carbon\Carbon::parse($row->created_at)->format('M d')}}</time>
-                        <span class="text">{{$row->note ?? 'No Note Attached'}} <a href="/estimate/{{$row->estimate->id}}/show">{{$row->estimate->eid ?? 'Unk'}} {{$row->estimate->customer->firstName}} {{$row->estimate->customer->lastName}}</a></span>
+                        <span class="text">{{$row->note ?? 'No Note Attached'}} <a href="/estimate/{{$row->estimate->id ?? ''}}/show">{{$row->estimate->eid ?? 'Unk'}} {{$row->estimate->customer->firstName}} {{$row->estimate->customer->lastName}}</a></span>
                     </li>
                 @endforeach
             </ol>
