d8346d6fc2acf7c680d91f941f71d7ed5d3638fa
emscomplete <Medic!283>
2020-10-03 10:40:35 -0400

    Initial Upload

--- resources/views/partials/messages2.blade.php
+++ resources/views/partials/messages2.blade.php
@@ -1,12 +1,34 @@
 
+@if(isset ($errors) && count($errors) > 0)
 
+    @foreach($errors->all() as $error)
+        <script>
+            //Notify
+            $.notify({
+                icon: 'fas fa-shield-check',
+                title: 'Action Completed',
+                message: '{{ $error }}',
+            },{
+                type: 'error',
+                placement: {
+                    from: "bottom",
+                    align: "right"
+                },
+                time: 5000,
+            });
+
+        </script>
+    @endforeach
+
+
+
+@endif
 @if(Session::get('success', false))
     <?php $data = Session::get('success'); ?>
     @if (is_array($data))
 
 
         @foreach ($data as $msg)
-
 
             <script>
 
