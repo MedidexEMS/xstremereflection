9f813eb47587a753dfdca3b7120176a792c0a8fe
emscomplete <Medic!283>
2020-10-11 17:58:03 -0400

    Initial Upload

---
+++ database/migrations/2020_10_11_215038_create_expenses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('companyId');
+            $table->integer('category')->nullable();
+            $table->string('description');
+            $table->decimal('total', 8, 2)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
