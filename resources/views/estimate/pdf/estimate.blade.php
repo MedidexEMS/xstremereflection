473ea36bd94bc17764036946c6a89fdd489f9472
joshblevins1985 <Medic!283>
2021-05-18 16:53:50 -0400

    Initial Upload

--- resources/views/estimate/pdf/estimate.blade.php
+++ resources/views/estimate/pdf/estimate.blade.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <title>Customer Estimate - PDF</title>
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <style>
         tr {
